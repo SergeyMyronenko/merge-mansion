6a3d3e5007ab83fbec565ad3c76acca48053eb4c
OlehKotov <mail.oleg456@gmail.com>
2024-12-21 13:13:47 +0200

    slider finish

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,49 +1,3 @@
-// import Swiper from 'swiper';
-// import 'swiper/css/bundle';
-
-// const leftArrow = document.getElementById('leftArrow');
-// const rightArrow = document.getElementById('rightArrow');
-
-// const gallerySwiper = new Swiper('.gallery-swiper-container', {
-//   direction: 'horizontal',
-//   loop: false,
-//   grabCursor: true,
-//   slidesPerView: 1,
-//   initialSlide: 0,
-//   spaceBetween: 2,
-//   grabCursor: true,
-//   allowTouchMove: true,
-//   speed: 500,
-//   breakpoints: {
-//     1200: {
-//       spaceBetween: 2,
-//     },
-//   },
-//   on: {
-//     init: () => {
-//       document.querySelector('.gallery-swiper-container').classList.add('show');
-//     },
-//     slideChange: () => {
-//       updateArrows();
-//     },
-//   },
-// });
-
-// function updateArrows() {
-//   leftArrow.disabled = gallerySwiper.isBeginning;
-//   rightArrow.disabled = gallerySwiper.isEnd;
-// }
-
-// updateArrows();
-
-// leftArrow.addEventListener('click', () => {
-//   gallerySwiper.slidePrev();
-// });
-
-// rightArrow.addEventListener('click', () => {
-//   gallerySwiper.slideNext();
-// });
-
 import Swiper from 'swiper';
 import 'swiper/css/bundle';
 
@@ -59,15 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
     initialSlide: 1,
     spaceBetween: 0,
     allowTouchMove: true,
-    speed: 500,
+    speed: 800,
     breakpoints: {
       1200: {
-        spaceBetween: 2,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        centeredSlides: true,
       },
     },
     on: {
       init: function () {
-        console.log('Swiper initialized');
         document
           .querySelector('.gallery-swiper-container')
           .classList.add('show');
@@ -75,10 +30,12 @@ document.addEventListener('DOMContentLoaded', () => {
       },
 
       slideChange: function () {
-        console.log('Slide changed to:', this.realIndex);
         updateArrows(this);
       },
     },
+    observer: true,
+    observeParents: true,
+    watchOverflow: true,
   });
 
   function updateArrows(swiperInstance) {

c64b89ae853ecee2d376d3c6e926d89196206ccb
Sergiy Myronenko <Sergiymyronenko.web@gmail.com>
2024-12-21 15:13:13 +0200

    update about section

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,6 +1,14 @@
+const moreBtn = document.querySelector('.more-button');
+const blurImages = document.querySelectorAll('.about-blur');
+
+moreBtn.addEventListener('click', readMore);
+blurImages.forEach(image => image.addEventListener('touchstart', onblurImage));
+blurImages.forEach(image =>
+  image.addEventListener('mouseover/mouseout', onblurImage)
+);
+
 function readMore() {
   const moreText = document.querySelector('.more-text');
-  const moreBtn = document.querySelector('.more-button');
 
   if (moreText.style.display === 'inline') {
     moreText.style.display = 'none';
@@ -9,4 +17,11 @@ function readMore() {
     moreText.style.display = 'inline';
     moreBtn.textContent = 'Close';
   }
+}
+
+function onblurImage(e) {
+  e.target.style.backdropFilter = 'none';
+  setTimeout(() => {
+    e.target.style.backdropFilter = '5px';
+  }, 2000);
 }
